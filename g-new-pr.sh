d2016d0ef25940f9bcf5d606f070d7efa92be022
Euclécio Josias <eucjosias@gmail.com>
2017-03-21 17:09:14 -0300

    wip

--- g-new-pr.sh
+++ g-new-pr.sh
@@ -110,13 +110,15 @@ printf "\n"
 
 data="{ \"title\": \"$title\", \"body\": \"$issue_desc \n\n$addinfo \n\n**Criado via CLI**\", \"head\": \"$originBranch\",  \"base\": \"$destinationBranch\" }"
 
-if [ -z ${GITHUB_TOKEN+x} ]; then
+if [ ! -z ${GITHUB_TOKEN+x} ]; then
+    request_return=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$repo_path/pulls -d "$data")
+    issue_url=$(echo ${request_return} | python -m json.tool | sed -n -e '/"issue_url":/ s/^.*"\(.*\)".*/\1/p')
+    curl -s -H "Authorization: token $GITHUB_TOKEN" "$issue_url/labels" -d "$stageLabel" >/dev/null
+else
     request_return=$(curl -s -X POST -H "Content-Type: application/json" -u $GITHUB_USER:$GITHUB_PASSWORD https://api.github.com/repos/$repo_path/pulls -d "$data")
     issue_url=$(echo ${request_return} | python -m json.tool | sed -n -e '/"issue_url":/ s/^.*"\(.*\)".*/\1/p')
-    echo $issue_url/labels
-    curl -s -H "Authorization: token $GITHUB_TOKEN" $issue_url/labels -d "$stageLabel"
-else
-    request_return=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/$repo_path/pulls -d "$data")
+    curl -s -H "Authorization: token $GITHUB_TOKEN" "$issue_url/labels" -d "$stageLabel" >/dev/null
+    curl -s -u $GITHUB_USER:$GITHUB_PASSWORD "$issue_url/labels" -d "$stageLabel" >/dev/null
 fi
 
 if [[ $request_return == *"Validation Failed"* ]]; then
